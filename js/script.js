a8307c773f073c9c0a0cd77ceddd72dac6231ff4
pannellalino <lista>
2022-10-10 15:20:13 +0200

    end_wow

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log('ciao');
+//console.log('ciao');
+
+const nomeUtente = prompt('Qual è il tuo nome?');
+const cognomeUtente = prompt('Qual è il tuo cognome?');
+const coloreUtente = prompt('Qual è il tuo colore preferito?');
+
+output = `
+ ${nomeUtente} ${cognomeUtente} ${coloreUtente} 22;
+`
+
+document.getElementById('prova').innerHTML += output;
